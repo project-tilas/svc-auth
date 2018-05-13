9517988ddab4ff226300eb693b2ae5e5ffc55064
Thomas Nairn <thomas@thenairn.com>
2018-05-13 22:56:16 +0100

    Fixed link on testGetGolang

--- main.go
+++ main.go
@@ -177,7 +177,7 @@ func getEnvVar(env string, fallback string) string {
 }
 
 func testGetGolang() {
-	response, err := http.Get("https://web-sniffer.net/")
+	response, err := http.Get("https://golang.org/")
 	if err != nil {
 		fmt.Printf("%s", err)
 		os.Exit(1)
